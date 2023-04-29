8ae61205aa3c4896e1783db401fcca2bebbda9da
Spandana <saispandana27@gmail.com>
2023-04-29 20:02:51 +0530

    Initial commit

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ app.get('/numbers', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
